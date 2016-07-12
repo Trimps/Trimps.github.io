2c571c1c657bfaf073f4b5f26b0f2b8a699a664a
Trimps <Trimpsgame@gmail.com>
2016-07-12 08:48:41 -0700

    fix

--- main.js
+++ main.js
@@ -458,7 +458,7 @@ function load(saveString, autoLoad, fromPf) {
         unlockMap(itemC);
     }
 	for (var messageBool in game.global.messages){
-		if (!game.global.messages[messageBool]){
+		if (!game.global.messages[messageBool].enabled){
 			filterMessage(messageBool, true);
 		}
 	}
