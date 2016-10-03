60adb732b136a19d03fbc232000c830112e6bec6
Victor Adam <victor@drawall.cc>
2016-10-03 22:24:16 +0200

    Fix for “Gym X2.00”

--- updates.js
+++ updates.js
@@ -1624,12 +1624,13 @@ function romanNumeral(number){
 }
 
 function prettifySub(number){
+	number = parseFloat(number);
 	var floor = Math.floor(number);
 	if (number === floor) // number is an integer, just show it as-is
 		return number;
 	var precision = 3 - floor.toString().length; // use the right number of digits
 
-	return parseFloat(number).toFixed(3 - floor.toString().length);
+	return number.toFixed(3 - floor.toString().length);
 }
 
 function resetGame(keepPortal) {
