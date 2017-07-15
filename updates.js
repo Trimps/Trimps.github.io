86dd294182343fc0640e1ad74905880932dbaff9
William Chow <431741580derp@gmail.com>
2017-07-15 10:47:08 +1000

    typo

--- updates.js
+++ updates.js
@@ -3273,7 +3273,7 @@ function settingTab(what){
 	searchSettings(elem);
 	clearSettingTabs();
 	var tabElem = document.getElementById(what + "Tab");
-	if (tabEzlem) swapClass('tab', 'tabSelected', tabElem);
+	if (tabElem) swapClass('tab', 'tabSelected', tabElem);
 	if (what == 'New') document.getElementById('NewTab').style.display = "table-cell";
 }
 
