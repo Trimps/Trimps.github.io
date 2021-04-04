90971b7e185689e236622c628b3b2c9ac8a39856
Trimps <Trimpsgame@gmail.com>
2021-04-03 19:28:10 -0700

    5.4.6

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ function newGame () {
 var toReturn = {
 	global: {
 		//New and accurate version
-		stringVersion: '5.4.5',
+		stringVersion: '5.4.6',
 		//Leave 'version' at 4.914 forever, for compatability with old saves
 		version: 4.914,
 		isBeta: false,
@@ -9509,7 +9509,7 @@ var toReturn = {
 		},
 		easterEgg: {
 			world: -1,
-			locked: true,
+			locked: false,
 			level: [0, 99],
 			title: "Colored Egg",
 			icon: "*droplet",
